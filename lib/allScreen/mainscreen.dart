ad65f66287dafcddc3c0b3ee450be92a3b2add48
Jeffery-Bernasko <57768306+Jeffery-Bernasko@users.noreply.github.com>
2021-08-26 22:24:35 +0000

    initial commit

--- lib/allScreen/mainscreen.dart
+++ lib/allScreen/mainscreen.dart
@@ -211,109 +211,114 @@ class _MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
             left: 0.0,
             right: 0.0,
             bottom: 0.0,
-            child: Container(
-              height: searchContainerHeight,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(18.0),
-                  topLeft: Radius.circular(18.0),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.white,
-                      blurRadius: 14.0,
-                      spreadRadius: 0.5,
-                      offset: Offset(0.7, 0.7))
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 24.0, vertical: 18.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 6.0,
-                    ),
-                    Text(
-                      "Hi There",
-                      style: TextStyle(fontSize: 12.0),
-                    ),
-                    Text(
-                      "Click Here for An Emergency Request",
-                      style: TextStyle(fontSize: 20.0),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-
-                    // Emergency Battery should be somewhere Here
-                    Center(
-                      child: MaterialButton(
-                        textColor: Colors.white,
-                        color: Colors.redAccent[400],
-                        child: Text('Emergency'),
-                        height: 50.0,
-                        onPressed: () async {
-                          var res = await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (content) => SearchScreen()));
-
-                          if (res == "obtainDirection") {
-                            await getPlaceDirection();
-                          }
-                        },
-                      ),
-                    ),
-                    // GestureDetector(
-                    // onTap: () {
-                    // Navigator.push(
-                    //   context,
-                    // MaterialPageRoute(
-                    //   builder: (context) => SearchScreen()));
-                    //},
-                    /* child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(5.0),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.black54,
-                                blurRadius: 6.0,
-                                spreadRadius: 0.5,
-                                offset: Offset(0.7, 0.7))
-                          ],
-                        ),*/
-                    // child: Padding(
-                    // padding: const EdgeInsets.all(8.0),
-                    //child: Row(
-                    //children: [
-                    /* Icon(
-                                Icons.search,
-                                color: Colors.black,
-                              ),
-                              SizedBox(
-                                width: 10.0,
-                              ),
-                              Text('Search Drop Off')*/
-                    //],
-                    //),
-                    //),
-                    //  ),
-
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    /* Divider(
-                      height: 1.0,
-                      color: Colors.black54,
-                      thickness: 1.0,
-                    ),*/
-                    // Home And Work Later
-                    //Row()
+            child: AnimatedSize(
+              vsync: this,
+              curve: Curves.bounceIn,
+              duration: new Duration(milliseconds: 160),
+              child: Container(
+                height: searchContainerHeight,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(18.0),
+                    topLeft: Radius.circular(18.0),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.white,
+                        blurRadius: 14.0,
+                        spreadRadius: 0.5,
+                        offset: Offset(0.7, 0.7))
                   ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 24.0, vertical: 18.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 6.0,
+                      ),
+                      Text(
+                        "Hi There",
+                        style: TextStyle(fontSize: 12.0),
+                      ),
+                      Text(
+                        "Click Here for An Emergency Request",
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+
+                      // Emergency Battery should be somewhere Here
+                      Center(
+                        child: MaterialButton(
+                          textColor: Colors.white,
+                          color: Colors.redAccent[400],
+                          child: Text('Emergency'),
+                          height: 50.0,
+                          onPressed: () async {
+                            var res = await Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (content) => SearchScreen()));
+
+                            if (res == "obtainDirection") {
+                              await getPlaceDirection();
+                            }
+                          },
+                        ),
+                      ),
+                      // GestureDetector(
+                      // onTap: () {
+                      // Navigator.push(
+                      //   context,
+                      // MaterialPageRoute(
+                      //   builder: (context) => SearchScreen()));
+                      //},
+                      /* child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5.0),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black54,
+                                  blurRadius: 6.0,
+                                  spreadRadius: 0.5,
+                                  offset: Offset(0.7, 0.7))
+                            ],
+                          ),*/
+                      // child: Padding(
+                      // padding: const EdgeInsets.all(8.0),
+                      //child: Row(
+                      //children: [
+                      /* Icon(
+                                  Icons.search,
+                                  color: Colors.black,
+                                ),
+                                SizedBox(
+                                  width: 10.0,
+                                ),
+                                Text('Search Drop Off')*/
+                      //],
+                      //),
+                      //),
+                      //  ),
+
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      /* Divider(
+                        height: 1.0,
+                        color: Colors.black54,
+                        thickness: 1.0,
+                      ),*/
+                      // Home And Work Later
+                      //Row()
+                    ],
+                  ),
                 ),
               ),
             ),
